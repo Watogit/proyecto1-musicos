670bfe1696588059e612f10adbe3c66a1b5b30ac
walter lopez <lopezpataquivawalter@artesyletras.edu.co>
2023-04-11 21:40:13 -0500

    introduccion de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto';
+  title = 'EscalApp';
+  edad= 35
+  pasaLaMateria: boolean= true;
+  dato: any= 46
+  contador: number= 0;
+
+
+  aumentar(){
+    this.contador=this.contador + 1;
+  }
+  disminuir(){
+    this.contador=this.contador - 1;
+  }
+  multiplicar(){
+    this.contador=this.contador *2;
+  }
+  dividir(){
+    this.contador=this.contador /2;
+  }
+  reset(){
+    this.contador=0;
+  }
+  esPar (numero: number):boolean{
+    if(numero % 2 === 0)/* === se utilizan para preguntar si es =*/
+    {return true;}
+    {return false;}
+  }
+  estudiantes:string[]=["lady","nath","walter","daniela","romario","alejandro","luisa"];
 }
+
+
